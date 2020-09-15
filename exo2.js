b9def868af2eafcfedc33453a3901806dcd0320f
moimarieblanc <moimarieblanc@gmail.com>
2020-09-15 12:26:29 +0200

    damien bonne couleur

--- exo2.js
+++ exo2.js
@@ -4,7 +4,7 @@ const ctx = myCanvas.getContext('2d');
 for(let i= 0; i<= 10; i++ ){
     for(let j= 0; j<= 10; j++ ){
         if ((i%2==0 && j%2 == 0) || (i%2!=0 && j%2 != 0)) {
-            ctx.fillStyle = 'blue';
+            ctx.fillStyle = '#008B8B';
         } else {
             ctx.fillStyle = 'white';
         }
