57a623702a0b3b92d88b9533a207543e52bc22ea
moimarieblanc <moimarieblanc@gmail.com>
2020-09-15 10:46:59 +0200

    texte et ombres

--- dessin.js
+++ dessin.js
@@ -1,6 +1,15 @@
 const canvas = document.getElementById('monCanvas');
 const ctx = canvas.getContext('2d');
 
+//Definition des ombres ---------------------------------------------------
+// Décalage de l'ombre selon X et Y
+ctx.shadowOffsetX = 2;
+ctx.shadowOffsetY = 2;
+// Etendue du flou
+ctx.shadowBlur = 4;
+// Couleur de l'ombre
+ctx.shadowColor = 'black';
+
 //tracé du rectangle rouge -----------------------------------------------------
 ctx.fillStyle = 'red';
 ctx.fillRect(10, 10, 200, 100);
@@ -37,6 +46,32 @@ ctx.beginPath();
 ctx.arc(80, 320, 40, 1.5*Math.PI , Math.PI , false);
 ctx.lineCap = 'square';
 ctx.strokeStyle = 'purple';
+ctx.stroke();
+
+//création d'un élément textuel  ---------------------------------------------
+// Contour des caractères seulement
+// ctx.strokeText(msg, x, y, [largeurMax]);
+// Caractères pleins
+// ctx.fillText(msg, x, y, [largeurMax]);
+
+const msg = '20°C';
+// Uniquement pour stokeText()
+// ctx.lineWidth = 2;
+ctx.font = '48px sans-serif';
+ctx.textAlign = 'left';
+ctx.textBaseline = 'top';
+const x = 250;
+const y = 180;
+ctx.fillText(msg, x, y, 600);
+
+let msgLength = ctx.measureText(msg).width;
+console.log(msgLength);
+ctx.beginPath();
+ctx.moveTo(x, y-10); //on indique le point de départ du tracé
+ctx.lineTo(x + msgLength, y-10); //on indique le point d'arrivée/passage du tracé 
+ctx.strokeStyle = 'green';
+ctx.lineWidth = 5;
+
 ctx.stroke();
 
 
