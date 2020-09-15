cc61c9eca4af7587da7158a95baf292e498ad40d
moimarieblanc <moimarieblanc@gmail.com>
2020-09-15 12:25:21 +0200

    trace damier

---
+++ exo2.js
@@ -0,0 +1,13 @@
+const myCanvas = document.getElementById('myCanvas');
+const ctx = myCanvas.getContext('2d');
+
+for(let i= 0; i<= 10; i++ ){
+    for(let j= 0; j<= 10; j++ ){
+        if ((i%2==0 && j%2 == 0) || (i%2!=0 && j%2 != 0)) {
+            ctx.fillStyle = 'blue';
+        } else {
+            ctx.fillStyle = 'white';
+        }
+        ctx.fillRect(i*60, j*60, 60, 60);
+    }
+}
